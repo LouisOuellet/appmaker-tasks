ed28a7a6976a99d1f78bf6f78a30df429bd4956e
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-20 15:41:24 -0500

    21.11-dev-4

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,13 @@
 API.Plugins.tasks = {
 	init:function(){
-		var html = '';
-		html += '<li class="nav-item">';
-			html += '<a class="nav-link" data-slide="true" href="#">';
-				html += '<i class="fas fa-tasks"></i>';
-			html += '</a>';
-		html += '</li>';
-		$('#navbar-right').prepend(html);
+		// var html = '';
+		// html += '<li class="nav-item">';
+		// 	html += '<a class="nav-link" data-slide="true" href="#">';
+		// 		html += '<i class="fas fa-tasks"></i>';
+		// 	html += '</a>';
+		// html += '</li>';
+		// $('#navbar-right').prepend(html);
+		API.GUI.Navbar.Task.init();
 	},
 }
 
